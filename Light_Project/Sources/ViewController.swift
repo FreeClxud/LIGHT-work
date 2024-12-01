904d0ec0e7f2d69106bcedc69ef5f61d400ba4da
FreeClxud <Romerom6470@durhamtech.edu>
2024-12-01 13:17:49 -0500

    Update ViewController.swift

--- Light_Project/Sources/ViewController.swift
+++ Light_Project/Sources/ViewController.swift
@@ -1,9 +1,34 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Manuel Romero on 11/10/24.
+//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    var lightOn = true
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateIU()
+        // Do any additional setup after loading the view.
+    }
+
+
+     func updateIU() {
+        
+         view.backgroundColor = lightOn ? .white : .black
+       
+          
+        
+    }
+    
+    @IBAction func BUTTON(_ sender: UIButton) {
+        lightOn.toggle()
+        updateIU()
     }
 }
